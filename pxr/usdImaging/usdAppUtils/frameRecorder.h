3c0de426b35ce766588dd3a53e646255e70038d4
Jon Creighton <jcreighton@apple.com>
2023-01-23 10:15:43 +0000

    Multiframe export

--- pxr/usdImaging/usdAppUtils/frameRecorder.h
+++ pxr/usdImaging/usdAppUtils/frameRecorder.h
@@ -130,8 +130,8 @@ public:
     bool Record(
             const UsdStagePtr& stage,
             const UsdGeomCamera& usdCamera,
-            const UsdTimeCode timeCode,
-            const std::string& outputImagePath, 
+            const std::vector<UsdTimeCode>& timeCode,
+            const std::vector<std::string>& outputImagePath, 
             const std::string& outputAOV);
 
 private:
