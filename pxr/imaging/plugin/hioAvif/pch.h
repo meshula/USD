fda765a4ac3cb2824690a4df38758b4ae2866dd4
Nick Porcino <meshula@hotmail.com>
2024-05-31 14:43:43 -0700

    Adding missing pch for hioAvif

---
+++ pxr/imaging/plugin/hioAvif/pch.h
@@ -0,0 +1,83 @@
+//
+// Copyright 2024 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+
+#define TF_MAX_ARITY 7
+#include "pxr/pxr.h"
+#include "pxr/base/arch/defines.h"
+#if defined(ARCH_OS_DARWIN)
+#include <mach/mach_time.h>
+#endif
+#if defined(ARCH_OS_LINUX)
+#include <x86intrin.h>
+#endif
+#if defined(ARCH_OS_WINDOWS)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+#include <intrin.h>
+#endif
+#include <algorithm>
+#include <atomic>
+#include <bitset>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <cmath>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <initializer_list>
+#include <iosfwd>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string>
+#include <sys/types.h>
+#include <thread>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#ifdef PXR_PYTHON_SUPPORT_ENABLED
+#if defined(__APPLE__) // Fix breakage caused by Python's pyport.h.
+#undef tolower
+#undef toupper
+#endif
+#endif // PXR_PYTHON_SUPPORT_ENABLED
+#include <tbb/atomic.h>
+#include <tbb/blocked_range.h>
+#include <tbb/cache_aligned_allocator.h>
+#include <tbb/concurrent_queue.h>
+#include <tbb/enumerable_thread_specific.h>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_for_each.h>
+#include <tbb/spin_mutex.h>
+#include <tbb/task.h>
